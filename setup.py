8b09dd479ed84c9c31cda607cb46c6bf766510e3
Itai Ganot <lel@lel.bz>
2025-02-26 18:46:00 +0200

    Release version 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python_copilot_docs_hook',
-    version='0.1.4',
+    version='0.1.5',
     description='A pre-commit hook to check Python documentation using GitHub Copilot',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
