d0c3dc81e50524454ed4fdc24510159e80c7b6a5
Itai Ganot <lel@lel.bz>
2025-02-26 20:26:55 +0200

    Release version 0.1.20

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python_copilot_docs_hook',
-    version='0.1.19',
+    version='0.1.20',
     packages=find_packages(),
     install_requires=[
         'openai>=1.0.0',
